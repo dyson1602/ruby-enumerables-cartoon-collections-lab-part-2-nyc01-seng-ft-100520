8d1a4176b4da26679a0b2f52ea9bd0e8b7f1fab3
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-17 19:51:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  planeteer_calls.each {|i| "#{i.upcase} + !"}
+  planeteer_calls.map {|i| "#{i.upcase} + !"}
 end
 
 def long_planeteer_calls(planeteer_calls)
