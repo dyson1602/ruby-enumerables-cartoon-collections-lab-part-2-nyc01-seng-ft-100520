2505b8a0405d307898ef5640277d1a9f41899f8b
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-17 20:07:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,7 @@ def find_valid_calls(planeteer_calls)
  
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  
-  planeteer_calls.find do |i|
-    if i ==
+  planeteer_calls.detect() 
     
     
     
